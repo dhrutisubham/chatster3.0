dc7f117c1991497f36e28e3b09916eb407800e78
dhrutisubham <dhruti.subham@gmail.com>
2024-12-06 05:51:38 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import { Button } from "@chakra-ui/react";
 import "./App.css";
-import { Route, Routes, Router } from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Chatpage from "./pages/Chatpage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
         <div className="App">
           <Route path="/" component={Homepage} exact />
           {/* since '/' is also include in '/chats' so both will be rendered in order to avoid this we are using exact */}
           <Route path="/chats" component={Chatpage} />
         </div>
-      </Routes>
-    </Router>
   );
 }
 
